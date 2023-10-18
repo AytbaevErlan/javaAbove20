dca0d84a12b763f4d67ce44b1a790331d9b65aab
https://github.com/AytbaevErlan <irlanaytbaev@gmail.com>
2023-10-18 14:49:17 +0200

    remaking

---
+++ src/main/java/lecture21/HttpServerLaucnher.java
@@ -0,0 +1,16 @@
+package main.java.lecture21;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class HttpServerLaucnher {
+    public static void main(String[] args) throws IOException {
+        HttpServer httpServer = HttpServer.create(new InetSocketAddress(8080),0);
+        httpServer.createContext("/hello", new HelloWorldHandler());
+        httpServer.createContext("/", new HelloWorldHandler());
+
+        httpServer.start();
+    }
+}
